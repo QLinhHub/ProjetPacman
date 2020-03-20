a4b974e6c3972bc95712b7977dc31d03bcadb473
Qlinhhub <lequanglinhez@gmail.com>
2020-03-20 12:56:28 +0200

    Read txt and eat fantom, not good at all

---
+++ pacman-with-qt-x64/pacmanwindow.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include "pacmanwindow.h"
+
+using namespace std;
+
+PacmanWindow::PacmanWindow(QWidget *pParent, Qt::WindowFlags flags):QFrame(pParent, flags)
+{
+    // Taille des cases en pixels
+    int largeurCase, hauteurCase;
+
+    if (pixmapPacman.load("./data/pacman.bmp")==false)
+    {
+        cout<<"Impossible d'ouvrir pacman.png"<<endl;
+        exit(-1);
+    }
+
+    if (pixmapFantome.load("./data/fantome.bmp")==false)
+    {
+        cout<<"Impossible d'ouvrir fantome.bmp"<<endl;
+        exit(-1);
+    }
+
+    if (pixmapMur.load("./data/mur.bmp")==false)
+    {
+        cout<<"Impossible d'ouvrir mur.bmp"<<endl;
+        exit(-1);
+    }
+
+    jeu.init();
+
+    QTimer *timer = new QTimer(this);
+    connect(timer, QTimer::timeout, this, PacmanWindow::handleTimer);
+    timer->start(100);
+
+    largeurCase = pixmapMur.width();
+    hauteurCase = pixmapMur.height();
+
+    resize(jeu.getNbCasesX()*largeurCase, jeu.getNbCasesY()*hauteurCase);
+}
+
+void PacmanWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    list<Fantome>::const_iterator itFantome;
+    int x, y;
+
+    // Taille des cases en pixels
+    int largeurCase, hauteurCase;
+
+    largeurCase = pixmapMur.width();
+    hauteurCase = pixmapMur.height();
+
+    // Dessine les cases
+    for (y=0;y<jeu.getNbCasesY();y++)
+        for (x=0;x<jeu.getNbCasesX();x++)
+			if (jeu.getCase(x,y)==MUR)
+                painter.drawPixmap(x*largeurCase, y*hauteurCase, pixmapMur);
+
+    // Dessine les fantomes
+    for (itFantome=jeu.fantomes.begin(); itFantome!=jeu.fantomes.end(); itFantome++)
+        painter.drawPixmap(itFantome->getPosX()*largeurCase, itFantome->getPosY()*hauteurCase, pixmapFantome);
+
+	// Dessine Pacman
+	painter.drawPixmap(jeu.getPacmanX()*largeurCase, jeu.getPacmanY()*hauteurCase, pixmapPacman);
+}
+
+void PacmanWindow::keyPressEvent(QKeyEvent *event)
+{
+    if (event->key()==Qt::Key_Left)
+        jeu.deplacePacman(GAUCHE);
+    else if (event->key()==Qt::Key_Right)
+        jeu.deplacePacman(DROITE);
+    else if (event->key()==Qt::Key_Up)
+        jeu.deplacePacman(HAUT);
+    else if (event->key()==Qt::Key_Down)
+        jeu.deplacePacman(BAS);
+    //Pacman mange un Fantom
+    for(auto it = jeu.fantomes.begin(); it != jeu.fantomes.end(); it++){
+        if(it->getPosX() == jeu.getPacmanX() && it->getPosY() == jeu.getPacmanY())
+            jeu.fantomes.erase(it);
+    }
+    update();
+}
+
+void PacmanWindow::handleTimer()
+{
+    jeu.evolue();
+    repaint();
+}
---
+++ pacman-with-qt-x64/pacmanwindow.h
@@ -0,0 +1,23 @@
+#ifndef PACMANWINDOW_H
+#define PACMANWINDOW_H
+
+#include <QtCore/QtCore>
+#include <QtWidgets/QtWidgets>
+#include "jeu.h"
+
+class PacmanWindow : public QFrame
+{
+  protected:
+    Jeu jeu;
+    QPixmap pixmapPacman, pixmapFantome, pixmapMur;
+
+  public:
+    PacmanWindow(QWidget *pParent=0, Qt::WindowFlags flags=0);
+    void handleTimer();
+
+  protected:
+    void paintEvent(QPaintEvent *);
+    void keyPressEvent(QKeyEvent *);
+};
+
+#endif
